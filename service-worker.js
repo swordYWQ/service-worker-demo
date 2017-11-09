67d7d9885e082d46edd04ed9854bb1d117d9e3fa
swordYWQ <865429505@qq.com>
2017-11-09 16:37:08 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -10,7 +10,6 @@ this.addEventListener('install', event => {
   event.waitUntil(
     caches.open(currentCache.offline).then(function(cache) {
       return cache.addAll([
-          './images/online.svg',
           './images/offline.svg',
           offlineUrl
       ]);
